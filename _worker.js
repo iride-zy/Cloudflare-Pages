d937e6dea15521eba36c4c83e35ef2f7064e9ec7
zhenyao xu <113428130+iride-zy@users.noreply.github.com>
2026-04-08 20:35:07 +0100

    Update fetch URL hostname for request handling

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="iridezy.alwaysdata.net";
+        url.hostname="iridezy.alwaysdata.net/57122179-e4d4-4958-9c9d-08cd5320df18.html";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
